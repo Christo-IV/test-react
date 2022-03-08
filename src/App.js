6f395b7d095296c3e9caa7789b519accdccd1954
Henry-Gerret Grüning <Henry-gerret.gruning@khk.ee>
2022-03-09 00:23:09 +0200

    Timetable

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import "./App.css";
 import Header from "./components/Header";
 import Landing from "./components/Landing";
 import ItPaths from "./components/ItPaths";
-
 import Career from "./components/Career";
-
 import Footer from "./components/Footer";
+import Timetable from "./components/Timetable";
 
 
 function App() {
@@ -16,6 +15,7 @@ function App() {
         <Landing />
         <ItPaths />
 <Career/>
+<Timetable/>
       </div>
       <Footer />
     </div>
---
+++ src/components/Timetable.js
@@ -0,0 +1,85 @@
+import React from "react";
+import "./css/Timetable.css";
+
+
+
+
+
+function Timetable() {
+    
+    return (
+        
+      <div>
+      <table>
+          <tr>
+          <th class="time">Aeg</th>
+          <th>Esmaspäev</th>
+          <th>Teisipäev</th>
+          <th>Kolmapäev</th>
+          <th>Neljapäev</th>
+          <th>Reede</th>
+          </tr>
+          <tr>
+              <td>07:00-08:29</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+          </tr>
+          <tr>
+              <td>08:30-10:00</td>
+              <td>mõistab veebirakenduste turvalsuse olulisust ning põhilisi ründevektoreid	</td>
+              <td>mõistab veebirakenduste turvalsuse olulisust ning põhilisi ründevektoreid; Kutsevõistlus	</td>
+              <td>Matemaatika; Kutsevõistlus</td>
+              <td>Matemaatika</td>
+              <td>mõistab infotehnoloogia mõju ja rolli ettevõtte juhtimises ning infoühiskonnas</td>
+          </tr>
+          <tr>
+              <td>10:15-11:45</td>
+              <td>mõistab veebirakenduste turvalsuse olulisust ning põhilisi ründevektoreid	</td>
+              <td>mõistab veebirakenduste turvalsuse olulisust ning põhilisi ründevektoreid; Kutsevõistlus	</td>
+              <td>kasutab serveripoolseid tehnoloogiaid veebirakenduste loomiseks; Kutsevõistlus	</td>
+              <td>kasutab serveripoolseid tehnoloogiaid veebirakenduste loomiseks	</td>
+              <td>Järgib ja kasutab agiilsete tarkvaraarendusmetoodikate põhimõtteid</td>
+          </tr>
+
+          <tr>
+              <td>11:55-14:00</td>
+              <td>Järgib ja kasutab agiilsete tarkvaraarendusmetoodikate põhimõtteid</td>
+              <td>Kutsevõistlus</td>
+              <td>kasutab serveripoolseid tehnoloogiaid veebirakenduste loomiseks; Kutsevõistlus	</td>
+              <td>kasutab serveripoolseid tehnoloogiaid veebirakenduste loomiseks	</td>
+              <td>Järgib ja kasutab agiilsete tarkvaraarendusmetoodikate põhimõtteid</td>
+          </tr>
+          <tr>
+              <td>14:10-15:40</td>
+              <td>Järgib ja kasutab agiilsete tarkvaraarendusmetoodikate põhimõtteid</td>
+              <td>Kutsevõistlus</td>
+              <td></td>
+              <td>Ettevalmistus matemaatika riigieksamiks</td>
+              <td></td>
+          </tr>
+          <tr>
+              <td>15:45-17:15</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td>Ettevalmistus matemaatika riigieksamiks</td>
+              <td></td>
+          </tr>
+          <tr>
+              <td>17:20-18:50</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+          </tr>
+      </table>
+
+      </div>
+    );
+  }
+  
+  export default Timetable;
