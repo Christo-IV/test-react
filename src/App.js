564487cbeeab4171aa8430313efe50983395e0f3
Henry-Gerret Grüning <Henry-gerret.gruning@khk.ee>
2022-03-08 23:40:23 +0200

    Fix

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import "./App.css";
 import Header from "./components/Header";
 import Landing from "./components/Landing";
 import ItPaths from "./components/ItPaths";
-<<<<<<< HEAD
+
 import Career from "./components/Career";
-=======
+
 import Footer from "./components/Footer";
->>>>>>> 78490fa3d1aa139648f91588607e28c923e90749
+
 
 function App() {
   return (
