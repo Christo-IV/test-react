daaa1df614e9cb077b51bc95384487ef37dd9973
Christo <christo.vahtramae@voco.ee>
2022-03-08 19:05:50 +0200

    Small header tweaks

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,28 +6,49 @@ function Header() {
   return (
     <header>
       <nav className="navbar top flex f-row">
-        <ul className="nav-imgs flex f-row">
-          <li className="nav-img">
-            <a>
-              <span className="fa fa-facebook"></span>
-            </a>
+        <ul className="nav-links flex f-row">
+          <li className="nav-link">
+            <a
+              href="https://www.facebook.com/tartuvoco/"
+              className="img-link fa fa-facebook"
+              target="_blank"
+              rel="noreferrer"
+            ></a>
           </li>
-          <li className="nav-img">
-            <span className="fa-brands fa-tiktok"></span>
+          <li className="nav-link">
+            <a
+              href="https://www.tiktok.com/@tartuvoco"
+              className="img-link fa-brands fa-tiktok"
+              target="_blank"
+              rel="noreferrer"
+            ></a>
           </li>
-          <li className="nav-img">
-            <span className="fa fa-instagram"></span>
+          <li className="nav-link">
+            <a
+              href="https://www.instagram.com/tartuvoco/"
+              className="img-link fa fa-instagram"
+              target="_blank"
+              rel="noreferrer"
+            ></a>
           </li>
         </ul>
         <ul className="nav-links flex f-row">
           <li className="nav-link">
-            <a href="#">Kontakt</a>
+            <a href="https://voco.ee/kontakt/" target="_blank" rel="noreferrer">
+              Kontakt
+            </a>
           </li>
           <li className="nav-link">
             <a href="#">Tunniplaan</a>
           </li>
           <li className="nav-link">
-            <a href="#">Siseveeb</a>
+            <a
+              href="https://siseveeb.voco.ee/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Siseveeb
+            </a>
           </li>
         </ul>
       </nav>
@@ -37,13 +58,17 @@ function Header() {
             <a href="#">Sisseastumine</a>
           </li>
           <li className="nav-link">
-            <a href="#">Erialad</a>
+            <a href="#itPaths">Erialad</a>
           </li>
           <li className="nav-link">
             <a href="#">Uudised</a>
           </li>
         </ul>
-        <img className="logo" src={logo} />
+        <img
+          className="logo"
+          src={logo}
+          alt="Tartu Rakendusliku Kolledži logo"
+        />
       </nav>
     </header>
   );
