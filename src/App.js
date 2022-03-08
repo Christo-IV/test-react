962d18a083bac31b65729157a493d3912576dd9a
Christo <christo.vahtramae@voco.ee>
2022-03-08 19:57:24 +0200

    Dynamic overview, missing imgs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Header from "./components/Header";
-import Overview from "./components/Overview";
 import Landing from "./components/Landing";
+import ItPaths from "./components/ItPaths";
 
 function App() {
   return (
@@ -9,12 +9,7 @@ function App() {
       <Header />
       <div className="container">
         <Landing />
-        <section className="page-section IT-paths flex f-center" id="itPaths">
-          <div class="grouping flex f-center">
-            <Overview />
-            <Overview />
-          </div>
-        </section>
+        <ItPaths />
       </div>
     </div>
   );
---
+++ src/components/ItPaths.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import Overview from "./Overview";
+import "./css/ItPaths.css";
+
+function ItPaths() {
+  const [specialities, setSpecialities] = useState([]);
+
+  function getSpecialities() {
+    fetch("data/specialities.json", {
+      headers: {
+        "Content-type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setSpecialities(data);
+      });
+  }
+
+  useEffect(() => {
+    getSpecialities();
+  }, []);
+
+  return (
+    <section className="page-section IT-paths flex f-center" id="itPaths">
+      <div class="grouping flex f-center">
+        {specialities.map((speciality, index) => {
+          return <Overview speciality={speciality} key={index} />;
+        })}
+      </div>
+    </section>
+  );
+}
+
+export default ItPaths;
