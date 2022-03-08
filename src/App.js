a987d5fbf1de46ed380ae75451c4c683e2f3f2bd
Christo <christo.vahtramae@voco.ee>
2022-03-08 13:24:12 +0200

    WIP commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <Header />
+      <section className="landing">
+        <img src="#" alt="IT akadeemia" />
+        <p className="introduction">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo
+          facere ea cumque saepe consequatur placeat delectus, architecto
+          perferendis atque non expedita voluptates necessitatibus quasi ullam
+          nemo modi maiores sunt repudiandae.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <img src="#" alt="IT robo bro" className="robo-bro" />
+      </section>
+      <section className=""></section>
     </div>
   );
 }
