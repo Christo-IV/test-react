dce5e9f0484c51d603eeaa49f8c7d97548e24eda
Christo <christo.vahtramae@voco.ee>
2022-03-08 15:17:01 +0200

    Added Overview component base

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,23 +1,26 @@
 import "./css/Overview.css";
+import placeholderImg from "../img/ITALOGO.jpg";
 
 function Overview() {
   return (
-    <div className="overview flex f-col f-center">
-      <div className="info flex f-col">
+    <div className="overview flex f-col">
+      <div className="info flex f-col shadow">
         <img
-          className="stock-img"
-          src="#"
+          className="stock-img shadow"
+          src={placeholderImg}
           alt="IT person in their natural habitat"
         />
-        <h2>&lt; Name of speciality here &gt;</h2>
-        <p className="desc">
+        <h2 className="speciality-name shadow">
+          &lt; Name of speciality here &gt;
+        </h2>
+        <p className="desc shadow">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum
           cumque distinctio voluptates dignissimos voluptatibus ullam expedita
           incidunt illum excepturi voluptate ipsa, nostrum dolorem animi
           recusandae perspiciatis corporis? Nostrum, mollitia pariatur.
         </p>
       </div>
-      <button className="apply-btn">Sisseastumine</button>
+      <button className="apply-btn shadow">Sisseastumine</button>
     </div>
   );
 }
